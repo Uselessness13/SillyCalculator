bef29f481ad789af7d2b2b8bf75a1ab52a72c104
Admin <admin@mac-admin.local>
2018-04-01 23:56:46 +0400

    Неужели все изменения нужно добавлять вручную!?????

--- SillyCalculator/ViewController.h
+++ SillyCalculator/ViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+double n,m;
+NSInteger operation;
+BOOL operationEnter;
+BOOL doubleOperation;
+enum {plus = 101, minus = 102, multi = 103, divi = 104};
+@interface ViewController : UIViewController {
+	IBOutlet UILabel *ekran;
+}
 
-@interface ViewController : UIViewController
-
+- (IBAction)clear:(id)sender;
+- (IBAction)ac:(id)sender;
+- (IBAction)num:(id)sender;
+- (IBAction)operation:(id)sender;
+- (IBAction)inverse:(id)sender;
 
 @end
 
--- SillyCalculator/ViewController.m
+++ SillyCalculator/ViewController.m
@@ -26,4 +26,70 @@
 }
 
 
+- (IBAction)clear:(id)sender{
+	n = 0;
+	[self show];
+}
+- (IBAction)ac:(id)sender{
+	n = 0;
+	
+	m = 0;
+	
+	operationEnter = NO;
+	
+	doubleOperation = NO;
+	
+	[self show];
+}
+- (IBAction)num:(id)sender{
+	if (operationEnter) {
+		m = n;
+		n = 0;
+		operationEnter = NO;
+	}
+	
+	n = (n * 10.0f) + [sender tag];
+	[self show];
+	NSLog(@"%i", [sender tag]);
+}
+- (IBAction)operation:(id)sender{
+	NSLog(@"%i", [sender tag]);
+	
+	if (doubleOperation && !operationEnter) {
+		switch (operation) {
+			case plus:
+				n = m + n;
+				break;
+			case minus:
+				n = m - n;
+				break;
+			case multi:
+				n = m * n;
+				break;
+			case divi:
+				n = m / n;
+				break;
+			default:
+				break;
+		}
+	}
+	m = n;
+	
+	operation = [sender tag];
+
+	operationEnter = YES;
+
+	doubleOperation = YES;
+	[self show];
+}
+- (IBAction)inverse:(id)sender{
+	n = -n;
+	[self show];
+}
+
+- (void) show {
+	NSString *str = [NSString stringWithFormat:@"%g",n];
+	[ekran setText:str];
+}
+
 @end
