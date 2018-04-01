c0a0da2025b39e60201acd8897292987fcef0a2f
Admin <admin@mac-admin.local>
2018-04-01 22:35:06 +0400

    Initial Commit

---
+++ SillyCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SillyCalculator
+//
+//  Created by Admin on 01.04.2018.
+//  Copyright © 2018 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SillyCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SillyCalculator
+//
+//  Created by Admin on 01.04.2018.
+//  Copyright © 2018 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
